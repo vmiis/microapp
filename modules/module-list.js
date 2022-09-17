4c3a2c82ec957d4ce47ea4c27b0b8f8a145e5b30
Zhiming Zhou <zzm.vmiis@gmail.com>
2022-09-17 19:45:23 +1000

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -4,8 +4,10 @@
     var H=$vm.hosting_path+"/modules";
     var m=$vm.module_list;
     //-------------------------------------------------------------------------------------
-    m[p+"revenue-2022-data"]              ={url:H+"/business/revenue-2022-data/page.html"};
-    m[p+"revenue-2022-chart"]             ={url:H+"/business/revenue-2022-chart/page.html"};
-    m[p+"student-data"]                   ={url:H+"/business/student-data/page.html",Table:"student",form_module:"student-form"};
-    m[p+"student-form"]                   ={url:H+"/business/student-form/page.html",Table:"student"};
+    m[p+"revenue-2022-data"]                        ={url:H+"/business/revenue-2022-data/page.html"};
+    m[p+"revenue-2022-chart"]                       ={url:H+"/business/revenue-2022-chart/page.html"};
+    m[p+"student-data"]                             ={url:H+"/business/student-data/page.html",Table:"student",form_module:"student-form"};
+    m[p+"student-form"]                             ={url:H+"/business/student-form/page.html",Table:"student"};
+    m[p+"What is a Micro-app"]                      ={url:H+"/business/What is a Micro-app/page.html",router:1};
+    m[p+"Cost of running a Micro-app"]              ={url:H+"/business/Cost of running a Micro-app/page.html",router:1};
 })();
