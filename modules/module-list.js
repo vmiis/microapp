87e20cd09ea5b18805e8bf5a6654796c51236358
Zhiming Zhou <zzm.vmiis@gmail.com>
2022-09-17 21:32:18 +1000

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -1,13 +1,16 @@
 (function(){
     //-------------------------------------------------------------------------------------
-    var p=""; //put a unique prefix to avoid conflict with others 
     var H=$vm.hosting_path+"/modules";
     var m=$vm.module_list;
     //-------------------------------------------------------------------------------------
-    m[p+"revenue-2022-data"]                        ={url:H+"/business/revenue-2022-data/page.html"};
-    m[p+"revenue-2022-chart"]                       ={url:H+"/business/revenue-2022-chart/page.html"};
-    m[p+"student-data"]                             ={url:H+"/business/student-data/page.html",Table:"student",form_module:"student-form"};
-    m[p+"student-form"]                             ={url:H+"/business/student-form/page.html",Table:"student"};
-    m[p+"What is a Micro-app"]                      ={url:H+"/business/What is a Micro-app/page.html",router:1};
-    m[p+"Cost of running a Micro-app"]              ={url:H+"/business/Cost of running a Micro-app/page.html",router:1};
+    m["revenue-2022-data"]                        ={url:H+"/business/*/page.html",router:1};
+    m["revenue-2022-chart"]                       ={url:H+"/business/*/page.html",router:1};
+    m["student-data"]                             ={url:H+"/business/*/page.html",Table:"student",form_module:"student-form",router:1};
+    m["student-form"]                             ={url:H+"/business/*/page.html",Table:"student"};
+    m["What is a Micro-app"]                      ={url:H+"/business/*/page.html",router:1};
+    m["Cost of running a Micro-app"]              ={url:H+"/business/*/page.html",router:1};
+    m["Authentication"]                           ={url:H+"/business/*/page.html",router:1};
+    m["Permissions Control"]                      ={url:H+"/business/*/page.html",router:1};
+    
+    for(p in m){  m[p].url=m[p].url.replace('*',p); }
 })();
