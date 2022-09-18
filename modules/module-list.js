8798b3a768d5edb3d2db10487920bff94a13d614
Zhiming Zhou <zzm.vmiis@gmail.com>
2022-09-18 14:29:21 +1000

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -5,12 +5,16 @@
     //-------------------------------------------------------------------------------------
     m["revenue-2022-data"]                        ={url:H+"/tasks/Use Cases/*/page.html",router:1};
     m["revenue-2022-chart"]                       ={url:H+"/tasks/Use Cases/*/page.html",router:1};
-    m["student-data"]                             ={url:H+"/tasks/Use Cases/*/page.html",Table:"student",form_module:"student-form",router:1};
-    m["student-form"]                             ={url:H+"/tasks/Use Cases/*/page.html",Table:"student"};
+    m["Student Data"]                             ={url:H+"/tasks/Use Cases/#/page.html",Table:"student",form_module:"Student Form",router:1};
+    m["Student Form"]                             ={url:H+"/tasks/Use Cases/#/page.html",Table:"student"};
     m["What is a Micro-app"]                      ={url:H+"/tasks/About Microapp/*/page.html",router:1};
     m["Cost of running a Micro-app"]              ={url:H+"/tasks/About Microapp/*/page.html",router:1};
     m["Authentication"]                           ={url:H+"/tasks/About Microapp/*/page.html",router:1};
     m["Permissions Control"]                      ={url:H+"/tasks/About Microapp/*/page.html",router:1};
     
-    for(p in m){  m[p].url=m[p].url.replace('*',p); }
+    for(p in m){  
+        m[p].url=m[p].url.replace('*',p); 
+        var a=p.substring(0,p.lastIndexOf(' ')) +'/'+p.substring(p.lastIndexOf(' ') + 1); //replace the last space to /, example: Student Form --> Student/Form
+        m[p].url=m[p].url.replace('#',a); 
+    }
 })();
